1cd950694a39486791c2070384a030a19298d8f4
Claudivan Moreira <claudivan.a.moreira@accenture.com>
2019-01-16 11:05:56 -0200

    Referencing JS Variables in JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Second way
+/*Second way
 function getButtonText() {
     return 'Click Me!';
 }
+Use: {getButtonText()}*/
 
 const App = () => {
     //First way: const buttonText = 'Click Me!';
     //Use: {buttonText}
+
+    //Third way:
+    const clickButton = {text: 'Click Me!'};
+    const labelName = {text: 'Enter Name:'};
+    
+    const styleClickButton = {backgroundColor: 'blue', color: 'white', border: '1px solid blue', padding: '5px 10px', borderRadius: '10px', outline: 'none', cursor: 'pointer'};
+    const styleInputName = {borderRadius: '10px', border: '1px solid #ccc', padding: '5px 10px', outline: 'none'};
+
     return (
         <div>
-            <label className="label" htmlFor="name">Enter Name:</label>
-            <input id="name" type="text"style={{borderRadius: '10px', border: '1px solid #ccc', padding: '5px 10px', outline: 'none'}}/>
-            <button style={{backgroundColor: 'blue', color: 'white', border: '1px solid blue', padding: '5px 10px', borderRadius: '10px', outline: 'none', cursor: 'pointer'}}>
-                {getButtonText()}
+            <label className="label" htmlFor="name">{labelName.text}</label>
+            <input id="name" type="text" style={styleInputName}/>
+            <button style={styleClickButton}>
+                {clickButton.text}
             </button>
         </div>
     )
