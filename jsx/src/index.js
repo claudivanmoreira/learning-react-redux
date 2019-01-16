538a3fb044427e674a5d090800fe0be033851db8
Claudivan Moreira <claudivan.a.moreira@accenture.com>
2019-01-16 10:42:39 -0200

    Styling jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi There!</div>
+    return (
+        <div>
+            <label class="label" for="name">Enter Name:</label>
+            <input id="name" type="text"style={{borderRadius: '10px', border: '1px solid #ccc', padding: '5px 10px', outline: 'none'}}/>
+            <button style={{backgroundColor: "blue", color: 'white', border: '1px solid blue', padding: '5px 10px', borderRadius: '10px', outline: 'none', cursor: 'pointer'}}>Submit</button>
+        </div>
+    )
 }  
 
 ReactDOM.render(<App/>, document.querySelector('#root') );
