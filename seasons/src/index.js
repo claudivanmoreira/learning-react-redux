40543296b745c33b4e841843c3b290105da97392
Claudivan Moreira <claudivan.a.moreira@accenture.com>
2019-01-23 19:13:12 -0200

    Cut update seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 /*const App = () => {
     window.navigator.geolocation.getCurrentPosition(
@@ -11,30 +12,26 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-    constructor(props) {
+    /*constructor(props) {
         console.log('[Mounting][Render phase] - Construtor chamado...');
         super(props);
-
         //THIS IS THE ONLY TIME we do direct assigment
         //to this.state
         this.state={ lat: null, errorMessage: '' };
+    }*/
 
-        window.navigator.geolocation.getCurrentPosition(
-            // we called setstate!!!
-            position => this.setState({ lat: position.coords.latitude}),
-            err => this.setState({ errorMessage: err.message})
-        );
-    }
+    //ALTERNATE STATE INITIALIZATION. The transpile will assign an constructor method to this class as before.
+    state={ lat: null, errorMessage: '' };
     
     // React says we have to define render!!
     render() {
         console.log('[Mounting][Render phase] - render chamado...');
         if(this.state.errorMessage && !this.state.lat) {
-            return <div>Latitude: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>
         }
 
         if(!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
         
         return <div>Loading!</div>
@@ -42,10 +39,17 @@ class App extends React.Component {
 
     componentDidMount() {
         console.log('[Mounting][Commit phase] - componentDidMount chamado...');
+        window.navigator.geolocation.getCurrentPosition(
+            // we called setstate!!!
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message})
+        );
     }
+
     componentDidUpdate() {
         console.log('[Updating][Commit phase] - componentDidUpdate chamado...');
     }
+
     componentWillUnpount() {
         console.log('[Unmounting][Commit phase] - componentWillUnpount chamado...');
     }
